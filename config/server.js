746331fc89347bb1fc0936ecee47cb01f515d8ca
Desterio Dennis <desteriodennis@live.com>
2020-12-16 16:58:16 +0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43fc8a4b744a3fb212384b2e13c90573'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4f64dc38-4145-4154-a855-ef3ca2bc96fb'
+};
